be96b11679b90cee798dca371a20fb750edcf496
dangthimongvui <56618854+dangthimongvui@users.noreply.github.com>
2019-10-15 23:33:05 -0700

    Delete CHAMTHI_controller.php

--- CHAMTHI_controller.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	require_once("C:/xampp/htdocs/luan_van/models/CHAMTHI_model.php");
-	$ct = new ChamThi();
-	session_start();
-	$chamthi = $ct->getChamThi($_SESSION["MaGV"]);
-	$chamthinotsession = $ct->getchamthinotsession();
-?>
