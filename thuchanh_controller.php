671b6044b8b5481687dcb50d5accbce4155ed2f1
dangthimongvui <56618854+dangthimongvui@users.noreply.github.com>
2019-10-15 23:35:13 -0700

    Delete thuchanh_controller.php

--- thuchanh_controller.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once("C:/xampp/htdocs/luan_van_ver2/models/Thuc_hanh_model.php");
-	$DKThucHanh = new ThucHanh();
-	$thuchanh = $DKThucHanh->getThucHanh();  
-	// var_dump($thuchanh);
-?>
