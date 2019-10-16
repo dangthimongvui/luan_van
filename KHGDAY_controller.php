ad57314a4878d4592c4002d82d10e9410534fdc9
dangthimongvui <56618854+dangthimongvui@users.noreply.github.com>
2019-10-15 23:37:01 -0700

    Delete KHGDAY_controller.php

--- KHGDAY_controller.php
+++
@@ -1,8 +0,0 @@
-<?php  
-	require_once("C:/xampp/htdocs/luan_van_ver2/models/KHGIANGDAY_model.php");
-	$khgd = new KHGiangDay();
-	$khgiangday = $khgd->getKHGDay();
-	// var_dump($khgiangday);
-	// echo "<script>alert('da luu du lieu');</script>";
-	// header("Location: http://localhost/luan_van/views/giang_vien/day.php");
-?>
