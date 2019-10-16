d7c1a7789f0f9ca817a06106ff3d185bd66c721b
dangthimongvui <56618854+dangthimongvui@users.noreply.github.com>
2019-10-16 00:09:36 -0700

    Add files via upload

---
+++ views/home/gioithieu.php
@@ -0,0 +1,20 @@
+<?php include("../../views/layout/header.php"); ?>
+<hr/></br></br></br>
+
+<div class="col-sm-12" style="text-align: center;border-style: outset;">
+	<h2 style="color: #9999FF">Sứ mạng, tầm nhìn, giá trị cốt lõi của </br>Trường Đại học Kỹ thuật - Công nghệ Cần Thơ</h2>
+	<p style="color:#FF9966">1. Sứ mạng:</p></br>
+		<b>Đào tạo nguồn nhân lực có trình độ chuyên môn, có kỹ năng nghề nghiệp tiếp cận với thực tiễn trong các lĩnh</br> vực kỹ thuật, công nghệ đáp ứng nhu cầu phát triển bền vững của thành phố Cần Thơ và vùng Đồng bằng sông Cửu Long. </b></br>
+	<p  style="color:#FF9966">2. Tầm nhìn:</p>  </br>
+		<b>Hướng tới năm 2030, Trường Đại học Kỹ thuật – Công nghệ Cần Thơ trở thành trường đại học phát triển bền vững, </br>o tạo có chất lượng theo hướng  nghiên cứu, thực hành ngang tầm các trường đại học khối ngành kỹ thuật – công nghệ trong nước. </b></br>
+
+	<p style="color:#FF9966">3. Giá trị cốt lõi:</p> </br>
+		<b>Chất lượng - Sáng tạo – Năng động - Phát triển bền vững</b>
+</div>
+<hr/>
+</br></br></br>
+<div>.</div>
+
+<?php 
+	include("../../views/layout/footer.php");
+?>
